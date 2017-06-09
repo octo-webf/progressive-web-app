abe8e142f65ef31285e6f0a90871214bf0ac91c1
Clément Dubois <cdubois@octo.com>
2017-06-09 17:25:42 +0200

    regiter sw on dev

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-console.log("aaaaaa");
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('assets')
+    .then(cache => cache.addAll([
+      '/progressive-web-app',
+    ])));
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log(event.request.url);
+});
