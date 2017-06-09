7b2caa6c3de0f40ffaaa6a965158cf9ee5034f66
Clément Dubois <cdubois@octo.com>
2017-06-09 18:16:14 +0200

    offline asset

--- sw.js
+++ sw.js
@@ -1,10 +1,19 @@
 self.addEventListener('install', (e) => {
+  console.log("install");
   e.waitUntil(caches.open('assets')
     .then(cache => cache.addAll([
-      '/progressive-web-app',
+      '/app.js',
+      '/',
     ])));
 });
 
 self.addEventListener('fetch', (event) => {
   console.log(event.request.url);
+  const response = caches.open('assets')
+    .then(cache => cache.match(event.request))
+    .then((cacheResponse) => {
+      console.log(cacheResponse);
+      return cacheResponse || fetch(event.request);
+    });
+  event.respondWith(response);
 });
