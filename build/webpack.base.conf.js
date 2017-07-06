f719bf93a085484358a1fea095501466bab8e060
Clément Dubois <cdubois@octo.com>
2017-07-06 12:43:24 +0200

    no more cache busting

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -62,7 +62,7 @@ module.exports = {
         loader: 'url-loader',
         query: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          name: utils.assetsPath('img/[name].[ext]')
         }
       },
       {
@@ -70,7 +70,7 @@ module.exports = {
         loader: 'url-loader',
         query: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name: utils.assetsPath('fonts/[name].[ext]')
         }
       }
     ]
