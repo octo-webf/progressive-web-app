8b9a683e9d9e4581da25e5b025d986c1d60ea472
Clément Dubois <cdubois@octo.com>
2017-07-06 16:40:12 +0200

    cache all assets

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,8 @@
 var express = require('express');
-var path = require('path');
 var app = express();
 
-// serve pure static assets
-var staticPath = path.posix.join('/', '/static');
-app.use(staticPath, express.static('./static'));
-app.use('/sw.js', express.static('./sw.js'));
+// serve static assets
+app.use('/', express.static(__dirname + '/'));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
--- dist/sw.js
+++ dist/sw.js
@@ -1,9 +1,15 @@
 self.addEventListener('install', (e) => {
-  console.log("install");
+  console.log('install');
   e.waitUntil(caches.open('assets')
     .then(cache => cache.addAll([
-      '/app.js',
-      '/',
+      '/static/js/app.js',
+      '/static/js/vendor.js',
+      '/static/css/app.css',
+      '/static/fonts/fontawesome-webfont.eot',
+      '/static/fonts/fontawesome-webfont.ttf',
+      '/static/fonts/fontawesome-webfont.woff',
+      '/static/fonts/fontawesome-webfont.woff2',
+      '/static/img/fontawesome-webfont.svg',
     ])));
 });
 
