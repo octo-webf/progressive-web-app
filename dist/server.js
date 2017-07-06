1ea8290edbd9639a76823605ae3b7c44e8e2eb2b
Clément Dubois <cdubois@octo.com>
2017-07-06 12:54:44 +0200

    serve static assets to root path

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname));
+var app = express();
+
+// serve pure static assets
+var staticPath = path.posix.join('/', '/static');
+app.use(staticPath, express.static('./static'));
+app.use('/sw.js', express.static('./sw.js'));
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
