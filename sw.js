92af78a3073fef1e19747b0fb77095c6f90788bb
Xavier J <xjulien@octo.com>
2017-07-28 15:27:50 +0200

    Add event push to get push notification in live

--- sw.js
+++ sw.js
@@ -25,3 +25,17 @@ self.addEventListener('fetch', (event) => {
     });
   event.respondWith(response);
 });
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
